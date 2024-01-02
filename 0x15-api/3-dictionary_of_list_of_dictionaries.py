3fcf6f4bf85a1e6908547ef25cdccb41efc53319
FouadEAF <fouad.azbi@gmail.com>
2024-01-02 14:00:00 -0800

    Using what you did in the task #0, extend your Python script to export data in the JSON format.

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,41 +4,32 @@
 """
 import json
 import requests
+import sys
 
 
 def export_all_to_json():
     url = 'https://jsonplaceholder.typicode.com/'
-    all_users_url = f'{url}users'
+    user = '{}users'.format(url)
+    res = requests.get(user)
+    json_o = res.json()
+    d_task = {}
+    for user in json_o:
+        name = user.get('username')
+        idUser = user.get('id')
+        todos = '{}todos?userId={}'.format(url, idUser)
+        res = requests.get(todos)
+        tasks = res.json()
+        l_task = []
+        for task in tasks:
+            dict_task = {"username": name,
+                         "task": task.get('title'),
+                         "completed": task.get('completed')}
+            l_task.append(dict_task)
 
-    all_users_data = requests.get(all_users_url).json()
-
-    all_data = {}
-
-    for user_data in all_users_data:
-        user_id = user_data['id']
-        username = user_data['username']
-
-        todos_url = f'{url}todos?userId={user_id}'
-        tasks_data = requests.get(todos_url).json()
-
-        user_tasks = []
-
-        for task in tasks_data:
-            user_tasks.append({
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            })
-
-        all_data[user_id] = user_tasks
-
-    # Export data to JSON file
-    output_file_name = 'todo_all_employees.json'
-
-    with open(output_file_name, 'w') as json_file:
-        json.dump(all_data, json_file, indent=2)
-
-    print("Data for all employees exported to {}".format(output_file_name))
+        d_task[str(idUser)] = l_task
+    filename = 'todo_all_employees.json'
+    with open(filename, mode='w') as f:
+        json.dump(d_task, f)
 
 
 if __name__ == "__main__":
